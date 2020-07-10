490af293ea2dc7d69ae10f4332973929026f68df
GuilhermeSalles <guibaltazarvs@gmail.com>
2020-07-10 11:39:23 -0300

    lambda2

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -4,10 +4,7 @@ public class Product {
 
 	private String name;
 	private Double price;
-
-	public Product() {
-	}
-
+	
 	public Product(String name, Double price) {
 		this.name = name;
 		this.price = price;
@@ -29,9 +26,16 @@ public class Product {
 		this.price = price;
 	}
 
+	public static boolean staticProductPredicate(Product p) {
+		return p.getPrice() >= 100.0;
+	}
+	
+	public boolean nonStaticProductPredicate() {
+		return price >= 100.0;
+	}
+	
 	@Override
 	public String toString() {
-		return "Product [name=" + name + ", price=" + price + "]";
+		return name + ", " + String.format("%.2f", price);
 	}
-
 }
